4a769af99eca1496c94686ab981b404c5bb3fe04
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-07-06 12:45:07 +0800

    Update build.gradle.kts

--- ZalithLauncher/build.gradle.kts
+++ ZalithLauncher/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.devtools.ksp")
+    id("com.google.devtools.ksp") version "2.2.0-1.0.0"
     id("kotlinx-serialization")
     id("stringfog")
 }
