1d2beafcbdd9b98049739126db4f4033fae97dee
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-02-10 11:38:07 +0800

    Enabled c23 features.

--- ZalithLauncher/src/main/jni/Android.mk
+++ ZalithLauncher/src/main/jni/Android.mk
@@ -37,7 +37,7 @@ LOCAL_SRC_FILES := \
     lwjgl_dlopen_hook.c
 
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
-LOCAL_CFLAGS += -DADRENO_POSSIBLE
+LOCAL_CFLAGS += -std=gnu23 -DADRENO_POSSIBLE
 LOCAL_LDLIBS += -lEGL -lGLESv2
 endif
 include $(BUILD_SHARED_LIBRARY)
