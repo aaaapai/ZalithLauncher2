a579edf2ef7d0fa3b8fda2294fdb57dc6ef51cdb
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-02-10 13:15:39 +0800

    Update environ.c

--- ZalithLauncher/src/main/jni/environ/environ.c
+++ ZalithLauncher/src/main/jni/environ/environ.c
@@ -7,12 +7,14 @@
 #include <assert.h>
 #include <string.h>
 #include "environ.h"
+#include "../jemalloc/jemalloc.h"
+
 struct pojav_environ_s *pojav_environ;
 __attribute__((constructor)) void env_init() {
     char* strptr_env = getenv("POJAV_ENVIRON");
     if(strptr_env == NULL) {
         __android_log_print(ANDROID_LOG_INFO, "Environ", "No environ found, creating...");
-        pojav_environ = malloc(sizeof(struct pojav_environ_s));
+        pojav_environ = je_malloc(sizeof(struct pojav_environ_s));
         assert(pojav_environ);
         memset(pojav_environ, 0 , sizeof(struct pojav_environ_s));
         if(asprintf(&strptr_env, "%p", pojav_environ) == -1) abort();
