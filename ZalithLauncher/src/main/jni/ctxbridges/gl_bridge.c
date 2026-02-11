64eac4ce62b98075b43452746e00f9d58d4b07db
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-02-11 10:12:43 +0800

    Update gl_bridge.c

--- ZalithLauncher/src/main/jni/ctxbridges/gl_bridge.c
+++ ZalithLauncher/src/main/jni/ctxbridges/gl_bridge.c
@@ -64,11 +64,10 @@ gl_render_window_t* gl_init_context(gl_render_window_t *share) {
                     EGL_RED_SIZE, 8,
                     EGL_ALPHA_SIZE, 8,
                     EGL_DEPTH_SIZE, 24,
-                    EGL_BUFFER_SIZE, 32,
                     EGL_SURFACE_TYPE,
                     EGL_WINDOW_BIT|EGL_PBUFFER_BIT,
                     EGL_RENDERABLE_TYPE,
-                    EGL_OPENGL_ES3_BIT|EGL_OPENGL_ES2_BIT|EGL_OPENGL_ES_BIT|EGL_OPENGL_BIT,
+                    EGL_OPENGL_ES2_BIT,
                     EGL_NONE
                     };
     EGLint num_configs = 0;
