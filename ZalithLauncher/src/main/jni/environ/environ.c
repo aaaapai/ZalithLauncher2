7609b9dca79e64244365c590cd56d49de59cdd07
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-02-12 21:54:30 +0800

    Update environ.c

--- ZalithLauncher/src/main/jni/environ/environ.c
+++ ZalithLauncher/src/main/jni/environ/environ.c
@@ -7,19 +7,31 @@
 #include <assert.h>
 #include <string.h>
 #include "environ.h"
+#include <android/native_window.h>
+#include <android/native_window_jni.h>
+#include <android/rect.h>
 #include "../jemalloc/jemalloc.h"
 
 struct pojav_environ_s *pojav_environ;
 __attribute__((constructor)) void env_init() {
     char* strptr_env = getenv("POJAV_ENVIRON");
     if(strptr_env == NULL) {
+        int deviceApiLevel = android_get_device_api_level();
         __android_log_print(ANDROID_LOG_INFO, "Environ", "No environ found, creating...");
-        pojav_environ = je_malloc(sizeof(struct pojav_environ_s));
+        if (deviceApiLevel <= 29 && !getenv("POJAV_FORCE_MALLOC_SYSTEM")) {
+          pojav_environ = je_malloc(sizeof(struct pojav_environ_s));
+        } else {
+          pojav_environ = malloc(sizeof(struct pojav_environ_s));
+        }
         assert(pojav_environ);
         memset(pojav_environ, 0 , sizeof(struct pojav_environ_s));
         if(asprintf(&strptr_env, "%p", pojav_environ) == -1) abort();
         setenv("POJAV_ENVIRON", strptr_env, 1);
-        je_free(strptr_env);
+        if (deviceApiLevel <= 29 && !getenv("POJAV_FORCE_MALLOC_SYSTEM")) {
+           je_free(strptr_env);
+        }else{
+           free(strptr_env);
+        }
     }else{
         __android_log_print(ANDROID_LOG_INFO, "Environ", "Found existing environ: %s", strptr_env);
         pojav_environ = (void*) strtoul(strptr_env, NULL, 0x10);
