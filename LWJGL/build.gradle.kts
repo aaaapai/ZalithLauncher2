789007c1fb26759f451313e645940d1ef2b41edc
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-07-27 14:58:50 +0800

    Update build.gradle.kts

--- LWJGL/build.gradle.kts
+++ LWJGL/build.gradle.kts
@@ -97,7 +97,4 @@ tasks.jar {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
-    }
 }
