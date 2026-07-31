be3efe31be2881f1cf9fef8222f43c59264918fb
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-07-31 22:44:08 +0800

    Update Android.mk

--- ZalithLauncher/src/main/jni/Android.mk
+++ ZalithLauncher/src/main/jni/Android.mk
@@ -5,17 +5,22 @@ HERE_PATH := $(LOCAL_PATH)
 # include $(HERE_PATH)/crash_dump/libbacktrace/Android.mk
 # include $(HERE_PATH)/crash_dump/debuggerd/Android.mk
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := log_stub
+LOCAL_SRC_FILES := log_stub.cpp
+LOCAL_CFLAGS += -O2 -D_FILE_OFFSET_BITS=64 -D_ALLBSD_SOURCE -DLINUX -D__USE_BSD -DANDROID -U_GNU_SOURCE -U__USE_GNU -std=c++17
+include $(BUILD_STATIC_LIBRARY)
 
 LOCAL_PATH := $(HERE_PATH)
 
 $(call import-module,prefab/bytehook)
 LOCAL_PATH := $(HERE_PATH)
 
-
 include $(CLEAR_VARS)
-LOCAL_LDLIBS := -ldl -llog -landroid
+LOCAL_LDLIBS := -ldl -landroid                         # 移除了 -llog
 LOCAL_MODULE := pojavexec
 LOCAL_SHARED_LIBRARIES := driver_helper
+LOCAL_STATIC_LIBRARIES := log_stub                     # 新增
 LOCAL_CFLAGS += -rdynamic
 LOCAL_SRC_FILES := \
     bigcoreaffinity.c \
@@ -42,11 +47,11 @@ LOCAL_LDLIBS += -lEGL -lGLESv2
 endif
 include $(BUILD_SHARED_LIBRARY)
 
-
 include $(CLEAR_VARS)
-LOCAL_LDLIBS := -ldl -llog
+LOCAL_LDLIBS := -ldl                                        # 移除了 -llog
 LOCAL_MODULE := vulkan_check
 LOCAL_SHARED_LIBRARIES := driver_helper
+LOCAL_STATIC_LIBRARIES := log_stub                          # 新增
 LOCAL_SRC_FILES := vulkan_checker.c
 LOCAL_CFLAGS += -O2 -D_FILE_OFFSET_BITS=64 -D_ALLBSD_SOURCE -DLINUX -D__USE_BSD -DANDROID -D_GNU_SOURCE -U__USE_GNU -std=c23
 include $(BUILD_SHARED_LIBRARY)
@@ -55,19 +60,19 @@ ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
 LOCAL_CFLAGS += -DADRENO_POSSIBLE
 endif
 
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := exithook
-LOCAL_LDLIBS := -ldl -llog
+LOCAL_LDLIBS := -ldl                                        # 移除了 -llog
 LOCAL_SHARED_LIBRARIES := bytehook pojavexec
+LOCAL_STATIC_LIBRARIES := log_stub                          # 新增
 LOCAL_SRC_FILES := exit_hook.c
 LOCAL_CFLAGS += -O2 -D_FILE_OFFSET_BITS=64 -D_ALLBSD_SOURCE -DLINUX -D__USE_BSD -DANDROID -D_GNU_SOURCE -U__USE_GNU -std=c23
 include $(BUILD_SHARED_LIBRARY)
 
-
 include $(CLEAR_VARS)
-LOCAL_LDLIBS := -ldl -llog -landroid
+LOCAL_LDLIBS := -ldl -landroid                              # 移除了 -llog
 LOCAL_MODULE := driver_helper
+LOCAL_STATIC_LIBRARIES := log_stub                          # 新增
 LOCAL_SRC_FILES := \
     driver_helper/driver_helper.c \
     driver_helper/nsbypass.c
@@ -80,7 +85,6 @@ LOCAL_LDLIBS += -lEGL -lGLESv3
 endif
 include $(BUILD_SHARED_LIBRARY)
 
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := linkerhook
 LOCAL_SRC_FILES := \
@@ -90,7 +94,6 @@ LOCAL_LDFLAGS := -z global
 LOCAL_CFLAGS += -O2 -D_FILE_OFFSET_BITS=64 -D_ALLBSD_SOURCE -DLINUX -D__USE_BSD -DANDROID -D_GNU_SOURCE -U__USE_GNU
 include $(BUILD_SHARED_LIBRARY)
 
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := pojavexec_awt
 LOCAL_SRC_FILES := \
@@ -98,11 +101,9 @@ LOCAL_SRC_FILES := \
 LOCAL_CFLAGS += -O2 -D_FILE_OFFSET_BITS=64 -D_ALLBSD_SOURCE -DLINUX -D__USE_BSD -DANDROID -U_GNU_SOURCE -U__USE_GNU -std=c23
 include $(BUILD_SHARED_LIBRARY)
 
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := awt_headless
 include $(BUILD_SHARED_LIBRARY)
-
 
 LOCAL_PATH := $(HERE_PATH)/awt_xawt
 include $(CLEAR_VARS)
@@ -112,8 +113,3 @@ LOCAL_SHARED_LIBRARIES := awt_headless
 LOCAL_SRC_FILES := xawt_fake.c
 LOCAL_CFLAGS += -O2 -D_FILE_OFFSET_BITS=64 -D_ALLBSD_SOURCE -DLINUX -D__USE_BSD -DANDROID -U_GNU_SOURCE -U__USE_GNU -std=c23
 include $(BUILD_SHARED_LIBRARY)
-
-
-# delete fake libs after linked
-$(info $(shell (rm $(HERE_PATH)/../jniLibs/*/libawt_headless.so)))
-
