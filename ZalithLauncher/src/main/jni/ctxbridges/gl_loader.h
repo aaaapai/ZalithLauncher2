b2ba4b1f92b0af1e8bd119278291ba08f6867018
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-07-31 23:19:33 +0800

    Create gl_loader.h

---
+++ ZalithLauncher/src/main/jni/ctxbridges/gl_loader.h
@@ -0,0 +1,19 @@
+#ifndef GL_LOADER_H
+#define GL_LOADER_H
+
+#include <EGL/egl.h>
+#include <stdbool.h>
+
+// GL函数指针声明
+typedef void (*glFlush_func)(void);
+typedef GLenum (*glGetError_func)(void);
+typedef const GLubyte* (*glGetString_func)(GLenum name);
+
+extern glFlush_func glFlush_p;
+extern glGetError_func glGetError_p;
+extern glGetString_func glGetString_p;
+
+// 加载GL函数
+bool dlsym_GL(void);
+
+#endif //GL_LOADER_H
